4fc1ae57cd60994f98892ff9460a79afd2653a26
SaiSayNoomLeng-haru <saisaynoomleng.haru.9723@gmail.com>
2024-08-11 22:58:50 -0500

    finished news homepage

--- junior/news-homepage/index.js
+++ junior/news-homepage/index.js
@@ -1,31 +1,33 @@
 const toggleBtn = document.querySelector('.mobile-nav-toggle');
 const navMenu = document.querySelector('#nav-menu');
-const coverImg = document.querySelector('#cover-img');
 const body = document.body;
+const img = document.querySelector('#cover-img');
 
 toggleBtn.addEventListener('click', ()=>{
     const visibility = navMenu.getAttribute('visible');
+
     if(visibility === 'false'){
         navMenu.setAttribute('visible', true);
         toggleBtn.setAttribute('aria-expanded', true);
-        body.style.backgroundColor = 'hsl(var(--clr-dark) / .3)';
+        body.style.backgroundColor = 'hsl(var(--clr-dark) / .2)'
     }
-    else{
+    else if (visibility === 'true'){
         navMenu.setAttribute('visible', false);
         toggleBtn.setAttribute('aria-expanded', false);
-        body.style.backgroundColor = 'hsl(var(--clr-white))';
+        body.style.backgroundColor = 'hsl(var(--clr-white)) '
     }
-});
+}); 
 
 function updateImg(){
-    const smallScreen = window.matchMedia('(max-width: 560px)');
-    const largeScreen = window.matchMedia('(min-width: 561px');
+    const smallScreen = window.matchMedia('(max-width: 35em)');
+    const largeScreen = window.matchMedia('(min-width: 35.1em)');
 
     if(smallScreen.matches){
-        coverImg.src = './assets/img/image-web-3-mobile.jpg';
+        img.src = "./assets/img/image-web-3-mobile.jpg";
     }else if(largeScreen.matches){
-        coverImg.src = '/assets/img/image-web-3-desktop.jpg';
+        img.src = "./assets/img/image-web-3-desktop.jpg"
     }
 }
 updateImg();
-window.addEventListener('resize', updateImg);
+
+window.addEventListener('resize', updateImg)
